d2390376393d78a00f073a37b25003c8b8a0ee6d
Merlin Eden <m.eden@ths-bs.de>
2023-11-19 23:35:40 +0100

    processor: Add dependencies

--- processor/build.gradle.kts
+++ processor/build.gradle.kts
@@ -1,11 +1,23 @@
-plugins {
-    kotlin("multiplatform")
-}
+plugins { kotlin("multiplatform") }
 
 kotlin {
     jvm()
-    js()
-    linuxX64()
-    linuxArm64()
-    mingwX64()
+
+	sourceSets {
+		jvmMain.dependencies {
+			implementation(project(":annotations"))
+			implementation(libs.ksp)
+			implementation(libs.bundles.kotlinpoet)
+		}
+
+		jvmTest.dependencies {
+			implementation(project(":annotations"))
+			implementation(testLibs.compile.testing)
+			implementation(testLibs.bundles.kotest)
+		}
+	}
+}
+
+tasks.withType<Test>().configureEach {
+	useJUnitPlatform()
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 rootProject.name = "DataInterfaces"
-include("annotations")
-include("processor")
+include(":annotations")
+include(":processor")
 
 pluginManagement {
     repositories {
@@ -12,8 +12,32 @@ pluginManagement {
 dependencyResolutionManagement {
     repositories { mavenCentral() }
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+    versionCatalogs { libs; testLibs }
 }
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
+
+val MutableVersionCatalogContainer.libs get() = create("libs") {
+    library("ksp", "com.google.devtools.ksp", "symbol-processing-api").version("1.9.20-1.0.14")
+
+    version("kotlinpoet", "1.15.1").also {
+        library("kotlinpoet", "com.squareup", "kotlinpoet").versionRef(it)
+        library("kotlinpoet-ksp", "com.squareup", "kotlinpoet-ksp").versionRef(it)
+    }
+
+    bundle("kotlinpoet", listOf("kotlinpoet", "kotlinpoet-ksp"))
+}
+
+val MutableVersionCatalogContainer.testLibs get() = create("testLibs") {
+	library("compile-testing", "com.github.tschuchortdev", "kotlin-compile-testing-ksp").version("1.5.0")
+
+	version("kotest", "5.8.0").also {
+		library("kotest-runner", "io.kotest", "kotest-runner-junit5").versionRef(it)
+		library("kotest-assertions", "io.kotest", "kotest-assertions-core").versionRef(it)
+	}
+
+	bundle("kotest", listOf("kotest-runner", "kotest-assertions"))
 }
